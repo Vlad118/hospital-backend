529a52f923007fccfd49526c39b5162fce6c8248
Vlad118 <s2187788@ed.ac.uk>
2023-03-04 21:47:31 +0000

    login patient, next task functionality; and can now send request to database

--- api.py
+++ api.py
@@ -1,37 +1,62 @@
 import os
 from main import Main
+from request import Request
 from flask import Flask, request, flash, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Home Page Route'
+    return 'Home Page Route' # Codrin what is the point of doing these things
 
 # get login information and return patient or nurse state
 @app.route('/api/login', methods = ['GET', 'POST'])
 def login_patient():
     selected_person = request.form.get('person')
     patient_id = request.form.get('id')
+    return_dict = []
+
     if selected_person == "NURSE":
         nurse_password = request.form.get('password')
-        
+
         if Main.check_nurse(nurse_id, nurse_password):
-            return 
+            nurse = Main.get_nurse_info()
+            return_dict['status'] = 202 # OK accepted nurse
+            return_dict['forename'] = nurse.forename
+            return_dict['surname'] = nurse.surname
         else:
-            return # allow for login retry how
-    else:
+            return_dict['status'] = 404 # error not found
+
+    else:   # patient
         if Main.check_patient_id(patient_id):
-            return selected_person
+            return_dict['status'] = 202 # OK accepted patient
         else:
-            return # allow for login retry how
+            return_dict['status'] = 404 # error not found
 
-@app.route('/api/login/nurse', methods = ['GET', 'POST'])
+    return return_dict # (status) for patient; (status, forename, surname) for nurse
+
+@app.route('/api/get_next_task', methods = ['GET', 'POST'])
 def get_next_task():
-    if request.form.get('break_active') == False: # ensuring BREAK button is not pressed
-        request_tuple = Main.get_next_request.getRequest() # tuple(patient_id, type_of_request, location)
+    task = Main.get_next_request() # tuple(patient_id, type_of_request, location)
+    return_dict = []
+    return_dict['patient_id'] = task.patient_id
+    return_dict['type_of_request'] = task.type_of_request
+    return_dict['location'] = task.location
+    return return_dict # (patient_id, type_of_request, location)
 
-    return # task information ?
+@app.route('/api/send_request', methods = ['GET', 'POST'])
+def send_request():                     # this needs the patient id as well !!!
+    type_of_request = request.form.get('type_of_request')
+    extra_info = request.form.get('extra_info')
+    patient_id = None # todo, codrin where can this be extracted?
+    location = None # completely unimplemented
+
+    request = Request()
+    request.patient_id = patient_id # todo
+    request.type_of_request = type_of_request
+    request.location = location # todo
+    request.extra_info = extra_info
+    
 
 # Run the application
 if __name__ == '__main__':
