5e08c95bf43d71b49a950b1a9bef86b4520892a5
Owen Pauptit <owenpauptit@gmail.com>
2023-03-04 16:27:48 +0000

    Added cursor attribute

--- database.py
+++ database.py
@@ -3,13 +3,14 @@ import sqlite3
 class Database:
     def __init__(self, db_file):
         self.db_file = db_file
+        self.cursor = None
 
-    def connect_db(db_file):
+    def connect_db(self, db_file):
         """ Creates a database connection with the SQLite database """
         connect = sqlite3.connect(db_file)
-        cursor = connect.cursor()
+        self.cursor = connect.cursor()
 
-        cursor.execute(
+        self.cursor.execute(
             '''
             CREATE TABLE IF NOT EXIST requests
             ([request_id] INTERGER PRIMARY KEY, [patient_id] INTEGER, 
@@ -17,11 +18,14 @@ class Database:
             '''
         ) #location within hospital (wing, ward...)
 
-        cursor.execute(
+        self.cursor.execute(
             '''
             CREATE TABLE IF NOT EXIST nurse
             ([nurse_id] INTEGER PRIMARY KEY, [forename] TEXT, [surname] TEXT,
               [email] TEXT, [password] TEXT, [location])'''
         ) #location is within hospital (to be able to link to patient)
+
+    def insert(self, ID, request):
+        self.cursor.execute()
 
 #TODO add connect.close() to close the database
