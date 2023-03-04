0b8adbc274a9fa4614a9bee22913a6696cc253ec
Vlad118 <s2187788@ed.ac.uk>
2023-03-04 20:04:04 +0000

    tuple form of Request class, login and next task base

--- api.py
+++ api.py
@@ -1,9 +1,37 @@
 import os
-from flask import Flask
+from main import Main
+from flask import Flask, request, flash, redirect
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def home():
+    return 'Home Page Route'
+
+# get login information and return patient or nurse state
+@app.route('/api/login', methods = ['GET', 'POST'])
+def login_patient():
+    selected_person = request.form.get('person')
+    patient_id = request.form.get('id')
+    if selected_person == "NURSE":
+        nurse_password = request.form.get('password')
+        if Main.check_nurse(nurse_id, nurse_password):
+            return selected_person
+        else
+            return # allow for login retry how
+    else:
+        if Main.check_patient_id(patient_id):
+            return selected_person
+        else:
+            return # allow for login retry how
+
+@app.route('/api/login/nurse', methods = ['GET', 'POST'])
+def get_next_task():
+    if request.form.get(break_active) == False: # ensuring BREAK button is not pressed
+        request_tuple = Main.get_next_request() # tuple(patient_id, type_of_request, location)
+
+    return # task information ?
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug = True)
--- request.py
+++ request.py
@@ -7,3 +7,11 @@ class Request:
         self.priority = None
         self.type_of_request = None
         self.location = None
+
+    def __getRequest__(self):
+        request_info = []
+        request_info.append(patient_id)
+        request_info.append(type_of_request)
+        request_info.append(location)
+
+        return tuple(request_info)                        
