398307de66a42967334903540786908083121230
Owen Pauptit <owenpauptit@gmail.com>
2023-03-04 16:34:49 +0000

    Request class

---
+++ request.py
@@ -0,0 +1,9 @@
+class Request:
+    """Class for request table"""
+
+    def __init__(self):
+        self.request_id = None
+        self.patient_id = None
+        self.priority = None
+        self.type_of_request = None
+        self.location = None
