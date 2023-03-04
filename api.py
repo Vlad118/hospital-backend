ba97d7aa727daa88da045a5f9ad6fde801d19263
Vlad118 <s2187788@ed.ac.uk>
2023-03-04 22:18:03 +0000

    comments, tidy up, just need patient_id retrieval mate

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import os
 from main import Main
 from request import Request
-from flask import Flask, request, flash, redirect
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def home():
     return 'Home Page Route' # Codrin what is the point of doing these things
 
-# get login information and return patient or nurse state
+# get login information and return state with nurse name if required
 @app.route('/api/login', methods = ['GET', 'POST'])
 def login_patient():
     selected_person = request.form.get('person')
@@ -46,10 +46,10 @@ def get_next_task():
 
 @app.route('/api/send_request', methods = ['GET', 'POST'])
 def send_request():                     # this needs the patient id as well !!!
-    type_of_request = request.form.get('type_of_request')
-    extra_info = request.form.get('extra_info')
     patient_id = None # todo, codrin where can this be extracted?
+    type_of_request = request.form.get('type_of_request')
     location = None # completely unimplemented, future project!
+    extra_info = request.form.get('extra_info')
 
     request = Request()
     request.patient_id = patient_id # todo
