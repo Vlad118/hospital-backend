d548616ee5a43a6b6e93a6c2a31c17e37d0993e5
licornes-fluos <54073764+licornes-fluos@users.noreply.github.com>
2023-03-04 16:20:08 +0000

    creation of tables

--- database.py
+++ database.py
@@ -1,18 +1,27 @@
 import sqlite3
-from sqlite3 import Error
 
-class Database():
+class Database:
+    def __init__(self, db_file):
+        self.db_file = db_file
 
     def connect_db(db_file):
         """ Creates a database connection with the SQLite database """
-        connect = None
-        try:
-            connect = sqlite3.connect(db_file)
-            print(sqlite3.version)
-        except Error as err:
-            print(err)
-        finally:
-            if connect:
-                connect.close()
+        connect = sqlite3.connect(db_file)
+        cursor = connect.cursor()
+
+        cursor.execute(
+            '''
+            CREATE TABLE IF NOT EXIST requests
+            ([request_id] INTERGER PRIMARY KEY, [patient_id] INTEGER, 
+            [priority] INTEGER, [type_of_request] TEXT, [location] TEXT)
+            '''
+        ) #location within hospital (wing, ward...)
+
+        cursor.execute(
+            '''
+            CREATE TABLE IF NOT EXIST nurse
+            ([nurse_id] INTEGER PRIMARY KEY, [forename] TEXT, [surname] TEXT,
+              [email] TEXT, [password] TEXT, [location])'''
+        ) #location is within hospital (to be able to link to patient)
 
 #TODO add connect.close() to close the database
